d02fbb46293ced8a50437b4f6df1275e8ce1eaa3
lcsdovalle <contatolucasthomas@gmail.com>
2020-08-30 13:31:14 -0300

    com atividades, turmas e professores

--- dash/models.py
+++ dash/models.py
@@ -53,3 +53,48 @@ class IaIndicadorProfessor(models.Model):
 
     def __str_(self):
         return self.nome
+
+class Turmas(models.Model):
+    name = models.CharField("Name", max_length=255,null=True,blank=True)
+    turma_id = models.CharField("id", max_length=100,unique=True)
+    owner_id = models.CharField('Owner', max_length=100)    
+    section = models.CharField('Section', max_length=255,null=True,blank=True)
+    owner_email = models.EmailField('Email')
+    class Meta:
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
+
+    def __str_(self):
+        return self.turma_id        
+
+class Atividade(models.Model):
+    title = models.CharField('Title', max_length=100,null=True,blank=True)
+    atividade_id = models.CharField('ID', max_length=200,unique=True)
+    due = models.DateTimeField('Due', auto_now=False, auto_now_add=False,null=True,blank=True)
+    state = models.CharField('Course State', max_length=50)
+    creator_id = models.CharField('Creator', max_length=100)
+    criado = models.CharField('Data criação', max_length=100)
+    atualizado = models.CharField('Data atualização', max_length=100)
+    turma_id = models.CharField('ID', max_length=200,default='00000')
+
+    class Meta:
+        verbose_name = ("Atividade")
+        verbose_name_plural = ("Atividades")
+
+    def __str_(self):
+        return self.atividade_id
+
+class Professor(models.Model):
+    nome = models.CharField('Name', max_length=255)
+    email = models.EmailField('Email', max_length=254,unique=True)
+    professor_id = models.CharField('Id', max_length=200,unique=True)
+    matricula = models.CharField('Matricula', max_length=50,null=True,blank=False)
+    cpf = models.CharField('Cpf', max_length=50,null=True,blank=False)
+    ultimo_acesso = models.CharField('Ultimo acesso', max_length=100)
+
+    class Meta:
+        verbose_name = ("Professor")
+        verbose_name_plural = ("Professores")
+
+    def __str_(self):
+        return self.nome
--- id_dispersao.py
+++ id_dispersao.py
@@ -20,68 +20,51 @@ report_service = authService(escopos,'jsons/cred_rs2.json',email='getedu@educar.
 # admin_service = authService(escopos,'jsons/cred_rs2.json',email='getedu@educar.rs.gov.br').getService('admin','directory_v1')
 
 if __name__ == "__main__":
-    E = {}
-    escolas = Escola.objects.all()
-    for escola in escolas:
-        E[escola.inep] = {}    
-        E[escola.inep]['inep'] = escola.inep    
-        E[escola.inep]['nome'] = escola.nome    
-        E[escola.inep]['municipio'] = escola.municipio    
-        E[escola.inep]['cre'] = escola.cre        
-        E[escola.inep]['total'] = 0 
-        E[escola.inep]['logaram'] = 0    
-
-    
     try:
-            args=[                   
-                'accounts:drive_used_quota_in_mb',
-                'accounts:first_name',
-                'accounts:last_name',
-                'accounts:timestamp_last_sso',
-            ]
-            dias = datetime.timedelta(days=3)    
-            odia = datetime.date.today() - dias
-            
-            started=False
+        args=[                   
+            'accounts:drive_used_quota_in_mb',
+            'accounts:first_name',
+            'accounts:last_name',
+            'accounts:timestamp_last_sso',
+            'classroom:num_courses_created',
+            'classroom:num_posts_created',
+            'classroom:role',
+            'classroom:timestamp_last_interaction'
+        ]
+        dias = datetime.timedelta(days=3)    
+        odia = datetime.date.today() - dias
+        
+        started=False
 
-            while True:
-                reports=""
-                if not started:
-                    reports = report_service.userUsageReport().get(
-                        date=odia.strftime('%Y-%m-%d'),
-                        userKey='all',
-                        parameters=','.join(args)                
-                        ).execute()
-                    started=True
-                    pagetoken = reports.get('nextPageToken')
-                else:
-                    reports = report_service.userUsageReport().get(
-                        date=odia.strftime('%Y-%m-%d'),
-                        userKey='all',
-                        parameters=','.join(args),
-                        pageToken=pagetoken            
+        while True:
+            reports=""
+            if not started:
+                reports = report_service.userUsageReport().get(
+                    date=odia.strftime('%Y-%m-%d'),
+                    userKey='all',
+                    parameters=','.join(args)                
                     ).execute()
+                started=True
+                pagetoken = reports.get('nextPageToken')
+            else:
+                reports = report_service.userUsageReport().get(
+                    date=odia.strftime('%Y-%m-%d'),
+                    userKey='all',
+                    parameters=','.join(args),
+                    pageToken=pagetoken            
+                ).execute()
 
-                    pagetoken = reports.get('nextPageToken',False)
-
-                nome = ""
-                ultimo_nome = ""
-                primeiro_nome = ""
-                uso_drive = 0
-                ultimo_acesso = ""
-                turmas_criadas = 0
-                posts_criados = 0
-                papel = 'student'
-                ultimo_acesso_classroom = 0    
-                
-                reports = reports.get('usageReports',None)
-                
-                if reports:
-                    for r in reports:
-                        report = r['parameters'] if r  and 'parameters' in r else r
-                        perfil = r['entity'] if r  and 'entity' in r else r                        
-                        
-        
-        
+                pagetoken = reports.get('nextPageToken',False)        
+            
+            reports = reports.get('usageReports',None)                    
+            if reports:
+                for r in reports:
+                    report = r['parameters'] if r  and 'parameters' in r else r
+                    perfil = r['entity'] if r  and 'entity' in r else r
+            
+            
+            if len(reports) <1000 or not pagetoken:
+                break
+            
     except Exception as e:
         print(e)
